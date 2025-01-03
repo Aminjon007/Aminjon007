9c861e901b16751c613c60cc39a0bcd291241725
Aminjon007 <aminjonjalilov4@gmail.com>
2025-01-03 13:59:01 +0500

    edited

--- Lesson1/Lesson1/obj/Debug/net9.0/Lesson1.AssemblyInfo.cs
+++ Lesson1/Lesson1/obj/Debug/net9.0/Lesson1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lesson1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fb0565b8ff61d8face698391babc3ceea5aaa54c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+456689094166089ea1872e15996ea1ef7329115e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lesson1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lesson1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
