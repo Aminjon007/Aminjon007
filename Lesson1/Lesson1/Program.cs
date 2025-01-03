6ac01f1f184f6d169977b23188a9ab3b3ae9d0e5
Aminjon007 <aminjonjalilov4@gmail.com>
2025-01-03 13:39:10 +0500

    lesson1

---
+++ Lesson1/Lesson1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace DZ1;
+
+class Program
+{
+    static void Main(string[] args)
+    { 
+        //___________begin1 
+        int a = 13;
+        int p = 4 * a;
+        Console.WriteLine("P= " + p);
+        //___________begin2
+        int a1 = 12;
+        int s = a1 * a1;
+        Console.WriteLine("S= " + s);
+        //___________integer1
+        int l = 12;
+        int l1 = l / 100;
+        Console.WriteLine("L= " + l1);
+        //___________integer2
+        int m = 1234;
+        int m1 = m / 1000;
+        Console.WriteLine("Ton= " + m1);
+    }
+}
