3ffaa455adeb4e0a19cf40ec9947febd97268f90
Aminjon007 <aminjonjalilov4@gmail.com>
2025-01-22 13:00:43 +0500

    Updated

---
+++ Lesson10/Lesson10/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Lesson10;
+
+class Program
+{
+    static void Main()
+    {
+        IMovable car = new Car();
+        car.Move(230);
+        Console.WriteLine();
+
+        IMovable robot = new Robot();
+        robot.Move(345);
+        Console.WriteLine();
+
+        Appliance appliance = new Refrigerator();
+        appliance.TurnOn();
+        appliance.TurnOff();
+        Console.WriteLine();
+
+        Appliance appliance1 = new WashingMachine();
+        appliance1.TurnOn();
+        appliance1.TurnOff();
+        Console.WriteLine();
+
+        Cat cat = new Cat();
+        cat.Eat();
+        cat.Sleep();
+        cat.Speak();
+        cat.Meow();
+        Console.WriteLine();
+
+        Dog dog = new Dog();
+        dog.Eat();
+        dog.Sleep();
+        dog.Speak();
+        dog.Bark();
+        Console.WriteLine();
+    }
+}
